994a01a0cc9fa094c5cacc564a9f00a2cbad69cd
weivegywei <weivegy.wei@gmail.com>
2021-07-16 22:24:42 +0300

    server config

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 5000, () => {console.log('running my server')});
+app.listen(process.env.PORT || 80, () => {console.log('running my server')});
 
 mongoose.connect(mongodbUrl, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log("mongodb connected")
